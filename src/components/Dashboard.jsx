63b3c35c9ade4e3fd95e99a608694d3c45e0a563
taimooraslam91 <taimooraslam91@gmail.com>
2023-06-23 17:21:58 +0500

    update ui

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,33 @@
 import ContractFunction from "./ContractFunctions";
+import { ReactComponent as Logo } from "../assets/images/logo.svg";
 
 function Dashboard({ connectedWallet, connectWallet }) {
   console.log("connected Wallet: ", connectedWallet);
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="dashboard">
+      <div className="header">
+        <div className="inner-div">
+          <a href="https://storagechain.io/">
+            <Logo className="logo" />
+          </a>
+          <a href="https://storagechain.io/sign-up" className="btn">
+            Sign up Free
+          </a>
+        </div>
+      </div>
+      <h1 className="title">Swap Token</h1>
       {connectedWallet?.walletAddress ? (
-        <div>
+        <div className="box-wrap">
           <p>Connected Wallet: {connectedWallet?.walletAddress}</p>
           <p>Balance: {connectedWallet?.balance}</p>
           <ContractFunction connectedWallet={connectedWallet} />
         </div>
       ) : (
-        <button onClick={async () => await connectWallet()}>
-          Connect your wallet
-        </button>
+        <div className="box-wrap">
+          <button onClick={async () => await connectWallet()}>
+            Connect your wallet
+          </button>
+        </div>
       )}
     </div>
   );
